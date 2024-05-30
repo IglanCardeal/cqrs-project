4b58d3a181f5f354e6770f325145d7005b9dac73
IglanCardeal <cmtcardeal@outlook.com>
2024-05-30 17:37:48 -0300

    feat: reset db for each server restart

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { existsSync, unlinkSync } from 'fs'
 
 async function bootstrap() {
+  const dbFile = 'db.sqlite'
+  if (existsSync(dbFile)) unlinkSync(dbFile)
+
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(
     new ValidationPipe({

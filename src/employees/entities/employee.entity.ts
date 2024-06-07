329320e7ea5d324c1dbc24cf7f36d9ea0534000b
IglanCardeal <cmtcardeal@outlook.com>
2024-06-06 22:25:37 -0300

    feat: use base entity to handle employees events

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -7,9 +7,14 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm'
 import { ContactInfo } from './contact-info.entity'
+import { EntityBase } from '@src/common/entity-base'
+import { ManagerAssignedEvent } from '../events/manager-assigned'
 
 @Entity('employees')
-export class Employee {
+export class Employee extends EntityBase {
+  #managerId?: number
+  #manager?: Employee
+
   @PrimaryGeneratedColumn()
   id: number
 
@@ -18,10 +23,23 @@ export class Employee {
 
   @ManyToOne(() => Employee, { onDelete: 'SET NULL' })
   @JoinColumn()
-  manager?: Employee
+  get manager(): Employee | undefined {
+    return this.#manager
+  }
+  set manager(value: Employee | undefined) {
+    this.managerId = value?.managerId
+    this.#manager = value
+  }
 
   @Column({ nullable: true })
-  managerId?: number
+  get managerId(): number | undefined {
+    return this.#managerId
+  }
+  set managerId(value: number | undefined) {
+    if (value && value !== this.#managerId)
+      this.addEvent(new ManagerAssignedEvent(this.id, value))
+    this.#managerId = value
+  }
 
   @OneToOne(() => ContactInfo, { cascade: true })
   @JoinColumn()

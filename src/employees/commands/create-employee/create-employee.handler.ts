9f21bce3feee56ce66bccc6ad2c088ab93fabcf3
IglanCardeal <cmtcardeal@outlook.com>
2024-06-06 21:10:23 -0300

    feat: emit manager assigned event when a new employee is created

--- src/employees/commands/create-employee/create-employee.handler.ts
+++ src/employees/commands/create-employee/create-employee.handler.ts
@@ -1,14 +1,18 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs'
 import { CreateEmployeeCommand } from './create-employee.command'
 import { DataSource } from 'typeorm'
 import { ContactInfo } from '@src/employees/entities/contact-info.entity'
 import { Employee } from '@src/employees/entities/employee.entity'
+import { ManagerAssignedEvent } from '@src/employees/events/manager-assigned'
 
 @CommandHandler(CreateEmployeeCommand)
 export class CreateEmployeeHandler
   implements ICommandHandler<CreateEmployeeCommand, number>
 {
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly eventBus: EventBus,
+  ) {}
 
   async execute(command: CreateEmployeeCommand): Promise<number> {
     return await this.dataSource.transaction(async (db) => {
@@ -18,6 +22,13 @@ export class CreateEmployeeHandler
         contactInfo,
       })
       await db.save(employee)
+
+      if (command.managerId) {
+        await this.eventBus.publish(
+          new ManagerAssignedEvent(employee.id, command.managerId),
+        )
+      }
+
       return employee.id
     })
   }
